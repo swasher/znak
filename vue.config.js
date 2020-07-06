8805c7771c92bd0a1a1614f1d0ae8c54221c76b0
Алексей Дзюба <alexeyd@pr.int>
2020-07-06 12:12:45 +0300

    work

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   assetsDir: 'static',
   outputDir: 'dist',
+  devServer: {
+    proxy: {
+      '^/*': {
+        target: 'http://127.0.0.1:8000/json',
+      },
+    },
+  },
 };
